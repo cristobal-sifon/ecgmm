2b3f4d661057382a440aebb19b0f7a5f4f27f843
jiangang.steven.hao <jiangang.steven.hao@1b43f4f8-a556-11de-be8d-735280eae207>
2011-04-28 19:04:53 +0000

    radially weighted ecgmm added and improved.

--- py/rwecgmmPy.py
+++ py/rwecgmmPy.py
@@ -1,9 +1,4 @@
 import numpy as np
-import pylab as pl
-import scipy as sp
-import scipy.integrate as itg
-import scipy.optimize as op
-import pyfits as pf
 import ecgmmPy as ec
 
 """
@@ -14,25 +9,24 @@ J. Hao
 4/20/2011
 
 """
-
 def poisson(r):
     const=2.
     res=const*r
     return res
 
-
 def nfw(r):
     rhos=104.5
-    rs=0.5
+    rs=0.15
     x=r/rs
     if type(x) is float:
         if x < 1:
             resf = 1-2./np.sqrt(1-x**2)*np.arctanh(np.sqrt((1-x)/(x+1)))
         if x > 1:
             resf = 1-2./np.sqrt(x**2-1)*np.arctan(np.sqrt((x-1)/(x+1)))
+            res = 2*rhos*rs/(x**2-1)*resf*r
         if x == 1:
             resf = 0.
-        res = 2*rhos*rs/(x**2-1)*resf
+        res = 2*rhos*rs/(x**2-1)*resf*r
     else:
         resf=np.zeros(len(x))
         lt1= x < 1
@@ -41,18 +35,49 @@ def nfw(r):
         resf[gt1] = 1-2./np.sqrt(x[gt1]**2-1)*np.arctan(np.sqrt((x[gt1]-1)/(x[gt1]+1)))
         eq1= x == 1
         resf[eq1] = 0.
-    res = 2*rhos*rs/(x**2-1)*resf*r
-    return res 
+    res=2*rhos*rs/(x**2-1)*resf*r
+    return res
 
+def nfwTruncted(r):
+    """
+    the nfw and poisson profile all normalized at r=3Mpc.
+    """
+    rhos=104.5/3.0287545869954959
+    rs=0.15
+    x=r/rs
+    if type(x) is float:
+        if x < 1:
+            resf = (1-2./np.sqrt(1.0001**2-1)*np.arctan(np.sqrt((1.0001-1)/(1.0001+1))))
+            res = 2*rhos*rs/(1.0001**2-1)*resf
+        if x > 1:
+            resf = 1-2./np.sqrt(x**2-1)*np.arctan(np.sqrt((x-1)/(x+1)))
+            res = 2*rhos*rs/(x**2-1)*resf*r
+        if x == 1:
+            resf = 0.
+            res = 2*rhos*rs/(x**2-1)*resf*r
+    else:
+        resf=np.zeros(len(x))
+        res=np.zeros(len(x))
+        xtmp=np.zeros(len(x))
+        lt1= x < 1
+        xtmp[lt1]=x[lt1]*0+1.0001
+        resf[lt1] = (1-2./np.sqrt(xtmp[lt1]**2-1)*np.arctan(np.sqrt((xtmp[lt1]-1)/(xtmp[lt1]+1))))
+        res[lt1] = 2*rhos*rs/(xtmp[lt1]**2-1)*resf[lt1]*xtmp[lt1]*rs
+        gt1= x > 1
+        resf[gt1] = 1-2./np.sqrt(x[gt1]**2-1)*np.arctan(np.sqrt((x[gt1]-1)/(x[gt1]+1)))
+        res[gt1] = 2*rhos*rs/(x[gt1]**2-1)*resf[gt1]*r[gt1]
+        eq1= x == 1
+        resf[eq1] = 0.
+        res[eq1]=0.
+    return res
 
 def pcz1(c,delta,r,mu1,sigma1):
     res=nfw(r)*np.exp(-(c-mu1)**2/2./(sigma1**2+delta**2))/np.sqrt(2.*3.1415926*(sigma1**2+delta**2))
     return res
- 
+
 def pcz2(c,delta,r,mu2,sigma2):
     res=poisson(r)*np.exp(-(c-mu2)**2/2./(sigma2**2+delta**2))/np.sqrt(2.*3.1415926*(sigma2**2+delta**2))
     return res
-   
 
 def pz1(c,delta,r,w1,w2,mu1,mu2,sigma1,sigma2):
     res=pcz1(c,delta,r,mu1,sigma1)*w1/(pcz1(c,delta,r,mu1,sigma1)*w1+pcz2(c,delta,r,mu2,sigma2)*w2)
@@ -99,20 +124,6 @@ def lkhood2(c,delta,r,w1,w2,mu1,mu2,sigma1,sigma2):
     res=sum(np.log(res))
     return res
 
-
-def munew(c,delta,r,mu,sigma):
-    upper=sum(c*sigma**2/(sigma**2+delta**2))
-    lower=sum(sigma**2/(sigma**2+delta**2))
-    res=upper/lower
-    return res
-
-def sigmanew(c,delta,r,mu,sigma):
-    upper=sum((c-mu1)**2*sigma**4/(sigma**2+delta**2)**2)
-    lower=sum(sigma**4/(sigma**2+delta**2)**2)
-    res=np.sqrt(upper/lower)
-    return res
-
-
 def bic1EM(c,delta,r):
     """
     Input: color, color errors, radius to the center
@@ -129,7 +140,7 @@ def bic2EM(c,delta,r,alpha,mu,sigma):
     Input: color, color errors, radius to the center, initial guess of alpha, mu and sigma
     Output: BIC,alpha,mu,sigma
     
-    """
+    """    
     w1=alpha[0]
     w2=alpha[1]
     mu1=mu[0]
@@ -157,4 +168,35 @@ def bic2EM(c,delta,r,alpha,mu,sigma):
             break
     BIC=-2.*lkhood_b + 5.*np.log(len(c))
     return BIC,alpha,mu,sigma
-            
+def nfwRandom(num=None):
+    """
+    generate random points following the nfw(r)
+    """
+    r=0.5
+    scale=5.
+    accept=0
+    reject=0
+    rr=np.zeros(num)
+    i=0
+    while accept < num:
+        rt=r+np.random.randn()*scale
+        if rt < 0 or rt > 1.:
+            continue
+        Ratio=min([1,nfw(rt)/nfw(r)])
+        U=np.random.rand()
+        if U <= Ratio:
+            rr[i] = rt
+            accept = accept + 1
+            i = i+1
+        else:
+            reject = reject +1
+    accRatio=accept/float(accept+reject)
+    return rr,accRatio
+
+def poissonRandom(num=None):
+    """
+    generate random points following the poisson(r)
+    """
+    u=np.random.rand(num)
+    r=np.sqrt(u)
+    return r
